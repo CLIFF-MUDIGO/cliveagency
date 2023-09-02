0f49c49d4e6e0201fc878aa968f1a3b8c9a08b3d
cliff <cliffmudigo5@gmail.com>
2023-09-02 11:18:52 +0300

    add nav paths

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Layout = ({children}) => {
+    const MenuItems =[
+        {
+            Title : "Home",
+            Path : "/",
+            Icon : "",
+        },
+        {
+            Title : "About",
+            Path :"/about",
+            Icon :"",
+        },
+        {
+            Title :"Clients",
+            Path : "/clients",
+            Icon : "",
+        },
+        {
+            Title : "Contact",
+            Path : "/contact",
+            Icon : "",
+        },
+    ]
   return (
     <div>
-        <div className="content">
+        {/*content*/}
+        <div className="">
             {children}
         </div>
-      <div className='footer'>
-        <h1>Footer</h1>
+        {/*footer*/}
+      <div className="fixed bottom-0 right-0 left-0"  >
+        <div className="flex">
+            {MenuItems.map((item)=>(
+                <div>
+                  <Link to={`${item.Path}`}>{item.Title}</Link>
+                </div>
+
+            ))}
+        </div>
+
       </div>
     </div>
   )
