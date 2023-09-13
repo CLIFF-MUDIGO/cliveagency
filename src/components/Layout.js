689f99ce3b4e589f0e09a0bd6fa42df0252275f0
cliff <cliffmudigo5@gmail.com>
2023-09-13 10:14:34 +0300

    layoutnavbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
   const MenuItems = [
     {
       Title: "Home",
       Path: "/",
-      Icon: "",
+      Icon: "ri-home-line",
     },
     {
       Title: "About",
       Path: "/about",
-      Icon: "",
+      Icon: "ri-information-line",
     },
     {
       Title: "Client",
       Path: "/client",
-      Icon: "",
+      Icon: "ri-user-line",
     },
     {
       Title: "Contact",
       Path: "/contact",
-      Icon: "",
+      Icon: "ri-contacts-fill",
     },
   ];
   return (
@@ -32,14 +33,28 @@ const Layout = ({ children }) => {
       <div className="fixed bottom-0 right-0 left-0">
         <div className="flex justify-center w-full ">
           {MenuItems.map((item, index) => (
-            <div
-              className={`px-20 bg-primary py-5 ${index === 0 && "rounded-l"}${
-                index === MenuItems.length - 1 && "rounded-r"
-              }`}
-            >
-              <Link to={`${item.Path}`} className="text-secondary">
-                {item.Title}
-              </Link>
+            <div className="flex flex-col justify-end items-center">
+
+              {location.pathname === item.Path && (<div className="flex flex-col items-center">
+                <div className="h-5 w-10 bg-primary rounded-t-full">
+
+                </div>
+                <div className="h-5 w-20 bg-primary rounded-t-full text-center">
+                  <i className={`${item.Icon} text-white text-xl`}></i>
+
+                </div>
+              </div>)}
+
+
+              <div
+                className={`px-20 bg-primary py-5 ${index === 0 ? "rounded-l" : ""}${index === MenuItems.length - 1 ? "rounded-r" : ""} flex items-center justify-center space-x-2 `}
+              >
+                {location.pathname !== item.Path && <i className={`${item.Icon} text-secondary text-xl`}></i>}
+
+                <Link to={`${item.Path}`} className="text-secondary text-xl">
+                  {item.Title}
+                </Link>
+              </div>
             </div>
           ))}
         </div>
